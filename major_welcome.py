f36f655aa374a0e0c55fbb9fd45a6a3520ae9ac5
EmmetEvans <e2evans@byu.edu>
2026-04-29 10:00:32 -0600

    Created major welcome message

---
+++ major_welcome.py
@@ -0,0 +1,4 @@
+#Ask for a user name, user major, and a short welcome message
+username = input("\nPlease enter your name: ")
+usermajor = input("Please enter your major: ")
+print(f"Hi, {username} from the prestigious {usermajor} program! Welcome to IS 303!\n")
